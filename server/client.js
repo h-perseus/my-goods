8abbeb0d8f4b7c1604162ecee68e97b5248fbecf
h-perseus <perseus362434@outlook.com>
2023-09-27 14:45:08 -0700

    sdfsdf

--- server/client.js
+++ server/client.js
@@ -13,7 +13,6 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use("/uploads", express.static("../uploads"));
 app.use(express.static("public"));
 
 app.use((req, res, next) => {
