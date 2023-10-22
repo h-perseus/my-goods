5e121ed6411a86685f2358a7d4c7a37fc7fb3a0b
h-perseus <perseus362434@outlook.com>
2023-10-22 13:42:38 -0700

    sdfsdf

--- server/admin.js
+++ server/admin.js
@@ -4,13 +4,13 @@ const app = express();
 const port = 8002; // Use the desired port
 
 // Serve static files from the 'build' directory
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
 // Handle API requests here (if your app has API endpoints)
 
 // Serve the React app for all other routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 app.listen(port, () => {
