0cf561a8e36c908e8baa370c4275bb7d7417879c
h-perseus <perseus362434@outlook.com>
2023-11-26 21:25:52 -0800

    sss

--- server/seed.js
+++ server/seed.js
@@ -4,22 +4,23 @@ const { Information, Admin, Domain, Product, Connection, Request, User } = requi
 const mongoURI = "mongodb://127.0.0.1:27017/my_goods";
 
 const ADMINS = [
-  { userId: "duyao1", password: "aa001314" },
-  { userId: "duyao2", password: "aa001314" },
-  { userId: "duyao3", password: "aa001314" },
-  { userId: "xiaoli1", password: "aa001314" },
-  { userId: "ming1", password: "aa001314" },
-  { userId: "ming2", password: "aa001314" },
-  { userId: "ming3", password: "aa001314" },
-  { userId: "ming4", password: "aa001314" },
+  // { userId: "duyao1", password: "aa001314" },
+  // { userId: "duyao2", password: "aa001314" },
+  // { userId: "duyao3", password: "aa001314" },
+  // { userId: "xiaoli1", password: "aa001314" },
+  // { userId: "ming1", password: "aa001314" },
+  // { userId: "ming2", password: "aa001314" },
+  // { userId: "ming3", password: "aa001314" },
+  // { userId: "ming4", password: "aa001314" },
   { userId: "ghost1", password: "aa001314" },
-  { userId: "ghost2", password: "aa001314" },
-  { userId: "ghost3", password: "aa001314" },
-  { userId: "ricky1", password: "aa001314" },
-  { userId: "ricky2", password: "aa001314" },
+  // { userId: "ghost2", password: "aa001314" },
+  // { userId: "ghost3", password: "aa001314" },
+  // { userId: "ricky1", password: "aa001314" },
+  // { userId: "ricky2", password: "aa001314" },
 ];
 
 const DOMAINS = [
+  { value: 'http://pay288-naver.com', status: '사용가능'},
   { value: 'http://pay-naver555.com', status: '사용가능'}
 ]
 
